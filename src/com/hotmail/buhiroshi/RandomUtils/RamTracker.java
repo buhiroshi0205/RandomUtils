28fa9d0c512f234ddd0d1caa1f267102a35868de
buhiroshi0205 <buhiroshi@hotmail.com>
2016-06-29 23:11:19 +0800

    Lots of uncommited changes: - /maintenance - /setcommand - RamTracker - backup players' inventories onQuit - more ranks support in /selfrank

---
+++ src/com/hotmail/buhiroshi/RandomUtils/RamTracker.java
@@ -0,0 +1,45 @@
+package com.hotmail.buhiroshi.RandomUtils;
+
+import java.util.logging.Level;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RamTracker implements Runnable {
+    
+    long[] max = new long[11];
+    long[] min = new long[11];
+    long[] sum = new long[11];
+    int[] numrecords = new int[11];
+    
+    JavaPlugin main;
+    
+    public RamTracker(JavaPlugin main) {
+        this.main = main;
+    }
+    
+    @Override
+    public void run() {
+        long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        int players = main.getServer().getOnlinePlayers().size();
+        if (players > 10) players = 10;
+        if (mem > max[players]) max[players] = mem;
+        if (min[players] == 0 || mem < min[players]) min[players] = mem;
+        sum[players] += mem;
+    }
+    
+    public void reset() {
+        max = new long[11];
+        min = new long[11];
+        sum = new long[11];
+        numrecords = new int[11];
+    }
+    
+    public void show() {
+        for (int i=0;i<11;i++) {
+            if (numrecords[i] != 0) {
+                main.getLogger().log(Level.INFO, "----- Players={0}: min={1}, max={2}, mean={3} -----",
+                        new Object[]{i, (int)(min[i]/1048576), (int)(max[i]/1048576), (int)(sum[i]/numrecords[i]/1048576)});
+            }
+        }
+    }
+    
+}
--- src/com/hotmail/buhiroshi/RandomUtils/RandomUtils.java
+++ src/com/hotmail/buhiroshi/RandomUtils/RandomUtils.java
@@ -1,30 +1,49 @@
 package com.hotmail.buhiroshi.RandomUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
+import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class RandomUtils extends JavaPlugin implements Listener {
     
+    boolean maintenance = false;
+    RamTracker ramtracker = new RamTracker(this);
+    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         try {
             if (!(sender instanceof Player)) {
-                sender.sendMessage("Command sender must be a player!");
+                if (label.equals("maintenance")) {
+                    maintenance = !maintenance;
+                    this.getLogger().log(Level.INFO, "Server maintenance status set to {0}!", maintenance);
+                } else {
+                    sender.sendMessage("Command sender must be a player!");
+                }
                 return true;
             }
             Player p = (Player) sender;
@@ -36,8 +55,8 @@ public class RandomUtils extends JavaPlugin implements Listener {
                     Bukkit.dispatchCommand(sender, "warp shop");
                     break;
                 case "selfrank":
-                    String[] check = {"NoodlesDragon", "PilipKim2010", "admin", "buhiroshi0205", "donor",
-                        "dragonslayer105", "helper", "member", "moderator", "novice", "srmember"};
+                    String[] check = {"NoodlesDragon", "PilipKim2010", "admin", "beta", "buhiroshi0205", "donor", "dragonslayer105", "elite",
+                        "helper", "legendary", "member", "moderator", "novice", "pro", "srmember", "titan", "ultimate", "veteran", "vip"};
                     if (Arrays.binarySearch(check, p.getName()) >= 0) {
                         if (args.length == 1 && Arrays.binarySearch(check, args[0]) >= 0) {
                             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " group set " + args[0]);
@@ -48,6 +67,33 @@ public class RandomUtils extends JavaPlugin implements Listener {
                     } else {
                         p.sendMessage("Unknown command. Type \"/help\" for help.");
                     }
+                    break;
+                case "setcommand":
+                    boolean success = false;
+                    if (p.hasPermission("RandomUtils.setcommand") && args.length > 0) {
+                        Set<Material> lalala = null;
+                        Block cmdblock = p.getTargetBlock(lalala, 10);
+                        if (cmdblock != null && cmdblock.getType() == Material.COMMAND) {
+                            StringBuilder sb = new StringBuilder(args[0]);
+                            for (int i=1;i<args.length;i++) {
+                                sb.append(' ');
+                                sb.append(args[i]);
+                            }
+                            CommandBlock state = (CommandBlock) cmdblock.getState();
+                            state.setCommand(sb.toString());
+                            state.update();
+                            p.sendMessage("Command set to: \"" + sb.toString() + "\"");
+                            success = true;
+                        }
+                    }
+                    if (!success) p.sendMessage("Failed! Are you sure you are looking directly at a command block 10 blocks within line of sight and entered a valid command?");
+                    break;
+                case "show":
+                    ramtracker.show();
+                    break;
+                case "dump":
+                    ramtracker.show();
+                    ramtracker.reset();
                     break;
             }
             return true;
@@ -60,6 +106,8 @@ public class RandomUtils extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, ramtracker, 6000, 6000);
+        getDataFolder().mkdir();
     }
     
     @EventHandler
@@ -70,6 +118,26 @@ public class RandomUtils extends JavaPlugin implements Listener {
                 if (loc.getBlock().isLiquid()) randomTP(e.getPlayer(), e.getTo().getWorld());
             }
         }
+    }
+    
+    @EventHandler
+    public void onPlayerLogin(AsyncPlayerPreLoginEvent e) {
+        if (!maintenance || e.getName().equals("buhiroshi0205")
+                || e.getName().equals("PilipKim2010")
+                || e.getName().equals("NoodlesDragon")
+                || e.getName().equals("dragonslayer105")) return;
+        e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "Server under Maintenance!");
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        File origin = new File("plugins" + File.separator + "PerWorldInventory" + File.separator + "data"
+                + File.separator + uuid.toString() + File.separator + "survival.json");
+        File destination = new File("plugins" + File.separator + "RandomUtils" + File.separator + e.getPlayer().getName() + ".json");
+        try {
+            Files.copy(origin.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {}
     }
     
     public void randomTP(Player p, World world) {
