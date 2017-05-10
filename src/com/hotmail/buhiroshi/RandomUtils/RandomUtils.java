ecf74789161f6fc43e2a36843099e20b0f1a00d4
buhiroshi0205 <buhiroshi@hotmail.com>
2017-05-10 17:26:43 +0800

    RandomUtils for lobby server

--- src/com/hotmail/buhiroshi/RandomUtils/RandomUtils.java
+++ src/com/hotmail/buhiroshi/RandomUtils/RandomUtils.java
@@ -1,22 +1,16 @@
 package com.hotmail.buhiroshi.RandomUtils;
 
-import java.util.Arrays;
-import java.util.Random;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -45,32 +39,9 @@ public class RandomUtils extends JavaPlugin implements Listener {
                         sender.sendMessage("Command sender must be a player!");
                         break;
                 }
-                return true;
+                return false;
             }
-            Player p = (Player) sender;
-            switch (label.toLowerCase()) {
-                case "spawn":
-                    Bukkit.dispatchCommand(sender, "warp spawn");
-                    break;
-                case "shop":
-                    Bukkit.dispatchCommand(sender, "warp shop");
-                    break;
-                case "selfrank":
-                    String[] check = {"NoodlesDragon", "PilipKim2010", "admin", "beta", "buhiroshi0205", "donor", "dragonslayer105", "elite",
-                        "helper", "legendary", "member", "moderator", "novice", "pro", "srmember", "titan", "ultimate", "veteran", "vip"};
-                    if (Arrays.binarySearch(check, p.getName()) >= 0) {
-                        if (args.length == 1 && Arrays.binarySearch(check, args[0]) >= 0) {
-                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " group set " + args[0]);
-                            p.sendMessage("Your rank is now: " + args[0]);
-                        } else {
-                            p.sendMessage("Unknown rank. Make sure that the input is all lower case.");
-                        }
-                    } else {
-                        p.sendMessage("Unknown command. Type \"/help\" for help.");
-                    }
-                    break;
-            }
-            return true;
+            return false;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -90,63 +61,17 @@ public class RandomUtils extends JavaPlugin implements Listener {
     }
     
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e) {
-        Location loc = e.getTo();
-        if (loc.getWorld().getName().equals("world") && loc.getBlockY() == 69) {
-            if (loc.getX() > 113 && loc.getX() < 120 && loc.getZ() > 75 && loc.getZ() < 82) {
-                if (loc.getBlock().isLiquid()) randomTP(e.getPlayer(), e.getTo().getWorld());
-            }
-        }
-    }
-    
-    @EventHandler
     public void onPlayerLogin(AsyncPlayerPreLoginEvent e) {
         if (!maintenance || e.getName().equals("buhiroshi0205")
-                || e.getName().equals("PilipKim2010")
+                || e.getName().equals("Veritas233")
                 || e.getName().equals("NoodlesDragon")
                 || e.getName().equals("dragonslayer105")) return;
         e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "Server under Maintenance!");
     }
     
-    /*@EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e) {
-        UUID uuid = e.getPlayer().getUniqueId();
-        File origin = new File("plugins" + File.separator + "PerWorldInventory" + File.separator + "data"
-                + File.separator + uuid.toString() + File.separator + "survival.json");
-        File destination = new File("plugins" + File.separator + "RandomUtils" + File.separator + e.getPlayer().getName() + ".json");
-        try {
-            Files.copy(origin.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException ex) {}
-    }*/
-    
-    public void randomTP(Player p, World world) {
-        int x = randomInRange(500, 2000);
-        int y;
-        int z = randomInRange(500, 2000);
-        Biome biome = world.getBiome(x, z);
-        if (biome == Biome.OCEAN || biome == biome.DEEP_OCEAN || biome == Biome.RIVER) return;
-        for (int i=0;i<5;i++) {
-            Block top = world.getHighestBlockAt(x, z);
-            y = top.getY();
-            if (top.isLiquid()) {} else {
-                if (top.getType().isSolid() || world.getBlockAt(x, --y, z).getType().isSolid()) {
-                    p.teleport(new Location(world, x+0.5, ++y, z+0.5));
-                    p.sendMessage(ChatColor.DARK_AQUA + "Teleported to the location:");
-                    p.sendMessage(ChatColor.DARK_AQUA + "X: " + ChatColor.AQUA + x);
-                    p.sendMessage(ChatColor.DARK_AQUA + "Y: " + ChatColor.AQUA + y);
-                    p.sendMessage(ChatColor.DARK_AQUA + "Z: " + ChatColor.AQUA + z);
-                    return;
-                }
-            }
-            x++;
-        }
-    }
-    
-    private int randomInRange(int min, int max) {
-        Random rnd = new Random();
-        int num = rnd.nextInt(max - min) + min;
-        if (rnd.nextBoolean()) num *= -1;
-        return num;
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.getPlayer().teleport(new Location(e.getPlayer().getWorld(), -340.5, 134, 134.5, -90, 0));
     }
     
 }
